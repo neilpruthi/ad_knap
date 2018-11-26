f3724e2eafdc2119afe96272b98e0a72dd64ec2d
ben-e <ben.ewing@duke.edu>
2018-11-26 18:53:31 -0500

    Updates to knapsack_functions_mat.R
    As well as updates to the ad_knap writeup.

--- r/knapsack_functions_mat.R
+++ r/knapsack_functions_mat.R
@@ -15,21 +15,14 @@ require(RcppAlgos)
 # For compiling functions to bytecode, this usually nets a tiny speedup
 require(compiler)
 
-# Testing ------------------------------------------------------------------------------------------
-# Number of draws from each item's dist
-n_draws <- 1000
-
-# Each col is an item, each row is a draw from the dist
-items <- matrix(c(
-  rnorm(n_draws, 0, 0),
-  rnorm(n_draws, 1, 0),
-  rnorm(n_draws, 2, 0),
-  rnorm(n_draws, 5, 2),
-  rnorm(n_draws, 4, 1),
-  rnorm(n_draws, 6, 1.257)
-), ncol = 6)
-
 # Functions ----------------------------------------------------------------------------------------
+
+# Helper to get number of unique characters in string
+nchar_unique <- function(s) {
+  length(unique(strsplit(s , "", fixed = TRUE)[[1]]))
+}
+nchar_unique <- cmpfun(nchar_unique)
+
 form_knapsacks <- function(items, size) {
   comboGeneral(1:ncol(items), m = size) %>% 
     apply(1, function(x) {
@@ -40,7 +33,18 @@ form_knapsacks <- function(items, size) {
     })
 }
 form_knapsacks <- cmpfun(form_knapsacks)
-knapsacks <- form_knapsacks(items, 2)
+
+# For possibly large knapsacks, just take a sample
+form_knapsack_sample <- function(items, size, n) {
+  comboSample(1:ncol(items), m = size, n = n) %>% 
+    apply(1, function(x) {
+      if (length(x) == 1) 
+        items[, x]
+      else 
+        rowSums(items[, x])
+    })
+}
+form_knapsack_sample <- cmpfun(form_knapsack_sample)
 
 estimate_knapsack_utility <- function(knapsacks, n_players) {
   nck <- ncol(knapsacks)
@@ -59,7 +63,7 @@ estimate_knapsack_utility <- function(knapsacks, n_players) {
         kkurtosis = kurtosis(knapsacks[ , unique(matchup)])
         
         data_frame(
-          knap_id = rep(unique(matchup), length(matchup)),
+          knap_id = unique(matchup),
           matchup = paste0(matchup, collapse = ";"),
           win_prop = 0, utility = 0,
           mean = kmean, 
@@ -78,11 +82,14 @@ estimate_knapsack_utility <- function(knapsacks, n_players) {
           kskewness = skewness(knapsacks[ , unique(matchup)])
           kkurtosis = kurtosis(knapsacks[ , unique(matchup)])
           
+          # if multiple players play the same winning strategy, they need to split the payout,
+          # but if they lose then they lose all
+          
           data_frame(
             knap_id = matchup[current_knap],
             matchup = paste0(matchup, collapse = ";"),
             win_prop = win_prop,
-            utility = ((n_players-1)*win_prop - 1*(1-win_prop)),
+            utility = (((n_players-1)*win_prop) - 1*(1-win_prop)),
             mean = kmean, 
             sd = ksd, 
             skewness = kskewness,
@@ -92,10 +99,7 @@ estimate_knapsack_utility <- function(knapsacks, n_players) {
       }
     })
 }
-
 estimate_knapsack_utility <- cmpfun(estimate_knapsack_utility)
-knapsacks_u2 <- estimate_knapsack_utility(knapsacks, 2)
-knapsacks_u3 <- estimate_knapsack_utility(knapsacks, 3)
 
 # Strategy functions -------------------------------------------------------------------------------
 # Given the knapsack (with estimated utilities), what knapsack would each function pick.
@@ -114,14 +118,16 @@ strat_minimax_2 <- function(knaps) {
   row_strats <- sort(unique(knaps$knap_id))
   col_strats <- sort(unique(knaps$knap_id))
   
-  MIPModel() %>%
-    add_variable(row_utility, type = "continuous") %>%
-    add_variable(row_pr[i], i = row_strats, type = "continuous", lb = 0, ub = 1) %>%
-    set_objective(row_utility, "max") %>%
-    add_constraint(sum_expr(row_pr[i]*knaps$utility[knaps$knap_id == i & knaps$matchup == paste0(sort(c(i, j)), collapse = ";")], i = row_strats) >= row_utility,
-                   j = col_strats) %>%
-    add_constraint(sum_expr(row_pr[i], i = row_strats) == 1) %>%
-    solve_model(with_ROI(solver = "glpk"))
+  suppressWarnings(
+    MIPModel() %>%
+      add_variable(row_utility, type = "continuous") %>%
+      add_variable(row_pr[i], i = row_strats, type = "continuous", lb = 0, ub = 1) %>%
+      set_objective(row_utility, "max") %>%
+      add_constraint(sum_expr(row_pr[i]*knaps$utility[knaps$knap_id == i & knaps$matchup == paste0(sort(c(i, j)), collapse = ";")], i = row_strats) >= row_utility,
+                     j = col_strats) %>%
+      add_constraint(sum_expr(row_pr[i], i = row_strats) == 1) %>%
+      solve_model(with_ROI(solver = "glpk"))
+  )
 }
 strat_minimax_2 <- cmpfun(strat_minimax_2)
 
@@ -130,14 +136,15 @@ strat_minimax_3 <- function(knaps) {
   
   strats <- sort(unique(knaps$knap_id))
   
-  MIPModel() %>%
-    add_variable(row_utility, type = "continuous") %>%
-    add_variable(row_pr[i], i = strats, type = "continuous", lb = 0, ub = 1) %>%
-    set_objective(row_utility, "max") %>%
-    add_constraint(sum_expr(row_pr[i]*knaps$utility[knaps$knap_id == i & knaps$matchup == paste0(sort(c(i, j, k)), collapse = ";")], i = strats) >= row_utility,
-                   j = strats, k = strats) %>%
-    add_constraint(sum_expr(row_pr[i], i = strats) == 1) %>%
-    solve_model(with_ROI(solver = "glpk"))
+  suppressWarnings(
+    MIPModel() %>%
+      add_variable(row_utility, type = "continuous") %>%
+      add_variable(row_pr[i], i = strats, type = "continuous", lb = 0, ub = 1) %>%
+      set_objective(row_utility, "max") %>%
+      add_constraint(sum_expr(row_pr[i]*knaps$utility[knaps$knap_id == i & knaps$matchup == paste0(sort(c(i, j, k)), collapse = ";")], i = strats) >= row_utility,
+                     j = strats, k = strats) %>%
+      add_constraint(sum_expr(row_pr[i], i = strats) == 1) %>%
+      solve_model(with_ROI(solver = "glpk"))
+  )
 }
 strat_minimax_3 <- cmpfun(strat_minimax_3)
-strat_minimax_3(knapsacks_u3)
---
+++ r/simulate_many_knapsacks.R
@@ -0,0 +1,52 @@
+# Description --------------------------------------------------------------------------------------
+# Simualte many knapsacks and save results.
+# Ben Ewing on 2018-11-26
+
+# Setup --------------------------------------------------------------------------------------------
+# For parallization
+library(future)
+library(furrr)
+plan(multiprocess)
+# For data manipulation
+library(dplyr)
+library(purrr)
+library(tidyr)
+# For kurtosis and skew functions
+library(e1071)
+# RcppAlgos::comboGeneral is ~80x faster than utils::combn in my quick test, it also seems to beat
+# data.table::CJ. It can also run in parallel, but this doesn't seem to help on Windows. Might be
+# worth trying on the Econ linux machines?
+library(RcppAlgos)
+# For compiling functions to bytecode, this usually nets a tiny speedup
+library(compiler)
+# For linear programming
+library(ROI)
+library(ROI.plugin.glpk)
+library(ompr)
+library(ompr.roi)
+
+source("r/knapsack_functions_mat.R")
+
+# Settings -----------------------------------------------------------------------------------------
+# sim_settings <- expand.grid(
+#   n_draws = 1000,
+#   n_players = 1:10,
+#   knap_size = 1:10,
+#   n_items = 10
+# ) %>% mutate(uid = 1:n())
+sim_settings <- expand.grid(
+  n_draws = 1000,
+  n_players = 2:3,
+  knap_size = 1:2,
+  n_items = 4
+) %>% mutate(uid = 1:n())
+
+# Simulations --------------------------------------------------------------------------------------
+future_pmap_dfr(sim_settings, function(n_draws, n_players, knap_size, n_items, uid) {
+  do.call(cbind, lapply(1:n_items, function(x) rnorm(n_draws, runif(1, 0, 10), runif(1, 0, 10)))) %>% 
+    form_knapsacks(knap_size) %>% 
+    estimate_knapsack_utility(n_players) %>% 
+    mutate(n_draws = n_draws, n_players = n_players, knap_size = knap_size, 
+           n_items = n_items, uid = uid)
+})
+
